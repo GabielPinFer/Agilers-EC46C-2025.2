efc9d4b549558e565118e600c01e0022ff37fa59
Pedrh316 <pedrh315@gmail.com>
2025-11-15 16:59:29 -0300

    implementação dos dados no localStorage e exposição de dados na Web

---
+++ Codigo/load.js
@@ -0,0 +1,13 @@
+import alunos from './alunos.json' with { type: 'json' };
+import professores from './professores.json' with { type: 'json' };
+import disciplinas from './disciplinas.json' with { type: 'json' };
+import materiais from './materiais.json' with { type: 'json' };
+
+export const load = () => {
+    localStorage.setItem('alunos', JSON.stringify(alunos));
+    localStorage.setItem('professores', JSON.stringify(professores));
+    localStorage.setItem('disciplinas', JSON.stringify(disciplinas));
+    localStorage.setItem('materiais', JSON.stringify(materiais));
+}
+
+load();
