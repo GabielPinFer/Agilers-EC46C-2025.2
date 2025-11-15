5970929b63f0bfb454ee5fb400124084985f3325
Pedrh316 <pedrh315@gmail.com>
2025-11-15 17:33:48 -0300

    implementado tela inicial de aluno cadastrado

--- Codigo/load.js
+++ Codigo/load.js
@@ -2,12 +2,14 @@ import alunos from './alunos.json' with { type: 'json' };
 import professores from './professores.json' with { type: 'json' };
 import disciplinas from './disciplinas.json' with { type: 'json' };
 import materiais from './materiais.json' with { type: 'json' };
+import login from './login.json' with { type: 'json' };
 
 export const load = () => {
     localStorage.setItem('alunos', JSON.stringify(alunos));
     localStorage.setItem('professores', JSON.stringify(professores));
     localStorage.setItem('disciplinas', JSON.stringify(disciplinas));
     localStorage.setItem('materiais', JSON.stringify(materiais));
+    localStorage.setItem('login', JSON.stringify(login));
 }
 
 load();
